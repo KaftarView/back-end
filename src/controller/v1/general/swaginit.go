fa6340d4e7e9fcfa96fdb6d1080e8329aadb9882
Roham Izadidoost <rohamizadidoost@Rohams-MacBook-Pro.local>
2024-11-16 22:13:45 +0330

    bug fixes

--- src/controller/v1/general/swaginit.go
+++ src/controller/v1/general/swaginit.go
@@ -4,7 +4,6 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// Response sends a standard JSON response with status and message
 func Response(c *gin.Context, statusCode int, message string, data interface{}) {
 	c.JSON(statusCode, SwaggerResponseMessage{
 		Status:  statusCode,
@@ -12,7 +11,6 @@ func Response(c *gin.Context, statusCode int, message string, data interface{}) 
 	})
 }
 
-// ResponseMessage represents a standard structure for API responses
 type SwaggerResponseMessage struct {
 	Status  int    `json:"status"`  // Status code (e.g., 200, 400, etc.)
 	Message string `json:"message"` // Message describing the response
