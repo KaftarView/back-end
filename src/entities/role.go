9a7f96d553e2d1d476b1872d3f11e5f80fdadb30
daryoush-safe <118970822+daryoush-safe@users.noreply.github.com>
2024-12-05 17:22:16 +0330

    fix: modify roles and set roles to users (#71)

--- src/entities/role.go
+++ src/entities/role.go
@@ -1,13 +1,11 @@
 package entities
 
 import (
-	"first-project/src/enums"
-
 	"gorm.io/gorm"
 )
 
 type Role struct {
 	gorm.Model
-	Type        enums.RoleType `gorm:"not null"`
-	Permissions []Permission   `gorm:"many2many:role_permissions;"`
+	Type        string       `gorm:"not null"`
+	Permissions []Permission `gorm:"many2many:role_permissions;"`
 }
--- src/routes/http/v1/user.go
+++ src/routes/http/v1/user.go
@@ -34,14 +34,10 @@ func SetupUserRoutes(routerGroup *gin.RouterGroup, di *bootstrap.Di, db *gorm.DB
 	}
 
 	users := routerGroup.Group("/users")
-	users.Use(authMiddleware.RequirePermission([]enums.PermissionType{enums.ManageUsers}))
+	users.Use(authMiddleware.RequirePermission([]enums.PermissionType{enums.ManageUsers, enums.ManageRoles}))
 	{
-		users.PUT("/add-roles", userController.UpdateUserRoles)
+		users.POST("/add-role", userController.CreateRole)
+		users.POST("/roles/:roleID/permissions", userController.UpdateRole)
+		users.POST("/:userID/roles", userController.UpdateUserRoles)
 	}
-
-	routerGroup.GET(
-		"/admin/hello",
-		authMiddleware.RequirePermission([]enums.PermissionType{enums.ManageUsers}),
-		userController.AdminSayHello,
-	)
 }
